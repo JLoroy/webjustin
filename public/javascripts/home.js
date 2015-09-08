6bb3dd696e428018dcf6ea58ceff9d0c8acc84e6
JLoroy <justin@loroy.be>
2015-09-08 21:31:23 +0200

    connection

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -5,4 +5,37 @@ app.controller('homeController', ['$scope', '$filter', '$http', function($scope,
     $scope.isActive = function(name){
         return name == $scope.activePanel
     }
+    $scope.connected=false;
+    $scope.password = "";
+    $scope.connect = function(){
+        if($scope.password == ""){
+            $scope.password="you need to put a password in here"
+        }
+        else{
+            $http.post('authorization', {password:$scope.password})
+                .then(function(res){
+                    $http.post('permName').then(function(res2){
+                        $scope.permName = res2.data.permName;
+                        if($scope.permName != "Stranger") $scope.connected = true;
+                        console.log(res2);
+                    });
+                });
+        }
+    };
+    $http.post('permName').then(function(res2){
+        $scope.permName = res2.data.permName;
+        if($scope.permName != "Stranger") $scope.connected = true;
+        console.log(res2);
+    });
+    $scope.disconnect= function(){
+        $http.post('disconnect')
+            .then(function(res){
+                $http.post('permName').then(function(res2){
+                    $scope.permName = res2.data.permName;
+                    if($scope.permName == "Stranger") $scope.connected = false;
+                    console.log(res2);
+                });
+            });
+    };
+
 }]);
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,75 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home' });
+    console.log("load home, isConnected?:"+req.session.permName);
+  res.render('home',{session:req.session});
+});
+
+router.post('/authorization', function(req,res,next){
+    var perm = 0;
+    var permName = 'Stranger';
+    console.log(req);
+    switch(req.body.password){
+        case 'RIPficus':
+            perm = 1;
+            permName = 'Friend';
+            break;
+        case '27avril':
+            perm = 2;
+            permName = 'Bro';
+            break;
+        case 'pandamour':
+            perm = 3;
+            permName = 'Namoureuse';
+            break;
+        case '0pa&0ma':
+            perm = 4;
+            permName = 'Moi-meme';
+            break;
+    }
+    sess = req.session;
+    sess.permission = perm;
+    sess.permName= permName;
+    console.log(permName + " connected");
+    res.end();
+});
+
+router.post('/disconnect', function(req,res,next){
+    sess = req.session;
+    sess.permission = 0;
+    sess.permName = 'Stranger';
+    console.log('disconnected');
+    res.end();
+});
+
+router.post('/permName', function(req,res,next){
+    sess = req.session;
+    console.log(sess);
+    if(req.session.permName){
+        res.send({permName:req.session.permName});
+    }
+    else{
+        res.send({permName:"Stranger"});
+    }
+
 });
 
 router.get('/juliette',  function(req, res, next) {
-  res.render('juliette', { title: 'Juliette' });
+    if(req.session.permission > 2){
+        res.render('juliette',{session:req.session});
+    }
+    else{
+        res.render('unauthorized',{});
+    }
 });
 
+
+router.get('/nico',  function(req, res, next) {
+    if(req.session.permission > 1){
+        res.render('nico',{session:req.session});
+    }
+    else{
+        res.render('unauthorized',{});
+    }
+});
 module.exports = router;
